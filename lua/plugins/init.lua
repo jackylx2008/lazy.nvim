10160903c5a1e6752cb96a69199c26fbc33d8096
Jackylx2008 <jackylx2008@hotmail.com>
2023-03-31 17:34:03 +0800

    add plugins

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -343,12 +343,39 @@ return {
 	},
 
 	-- Jumps and Motions
+	-- easily jump to any location and enhanced f/t motions for Leap
+	{
+		"ggandor/flit.nvim",
+		keys = function()
+			---@type LazyKeys[]
+			local ret = {}
+			for _, key in ipairs({ "f", "F", "t", "T" }) do
+				ret[#ret + 1] = { key, mode = { "n", "x", "o" }, desc = key }
+			end
+			return ret
+		end,
+		opts = { labeled_modes = "nx" },
+	},
 	{
 		"ggandor/leap.nvim",
-		keys = { "s", "S" },
-		config = function()
+		-- keys = { "s", "S" },
+		-- config = function()
+		-- 	local leap = require("leap")
+		-- 	leap.add_default_mappings()
+		-- end,
+		keys = {
+			{ "s", mode = { "n", "x", "o" }, desc = "Leap forward to" },
+			{ "S", mode = { "n", "x", "o" }, desc = "Leap backward to" },
+			{ "gs", mode = { "n", "x", "o" }, desc = "Leap from windows" },
+		},
+		config = function(_, opts)
 			local leap = require("leap")
-			leap.add_default_mappings()
+			for k, v in pairs(opts) do
+				leap.opts[k] = v
+			end
+			leap.add_default_mappings(true)
+			vim.keymap.del({ "x", "o" }, "x")
+			vim.keymap.del({ "x", "o" }, "X")
 		end,
 	},
 	{
@@ -397,7 +424,42 @@ return {
 		"potamides/pantran.nvim",
 		cmd = "Pantran",
 	},
+	-- references
+	{
+		"RRethy/vim-illuminate",
+		providers = {
+			"lsp",
+			"treesitter",
+			"regex",
+		},
+		event = { "BufReadPost", "BufNewFile" },
+		opts = { delay = 100 },
+		config = function(_, opts)
+			require("illuminate").configure(opts)
 
+			local function map(key, dir, buffer)
+				vim.keymap.set("n", key, function()
+					require("illuminate")["goto_" .. dir .. "_reference"](false)
+				end, { desc = dir:sub(1, 1):upper() .. dir:sub(2) .. " Reference", buffer = buffer })
+			end
+
+			map("]]", "next")
+			map("[[", "prev")
+
+			-- also set it after loading ftplugins, since a lot overwrite [[ and ]]
+			vim.api.nvim_create_autocmd("FileType", {
+				callback = function()
+					local buffer = vim.api.nvim_get_current_buf()
+					map("]]", "next", buffer)
+					map("[[", "prev", buffer)
+				end,
+			})
+		end,
+		keys = {
+			{ "]]", desc = "Next Reference" },
+			{ "[[", desc = "Prev Reference" },
+		},
+	},
 	-- Ui Noice
 	-- experimental
 	{
