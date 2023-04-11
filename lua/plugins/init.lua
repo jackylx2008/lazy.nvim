538ca67cce2d7cc9bdeed40ae2ab70a6429f971b
Jackylx2008 <jackylx2008@hotmail.com>
2023-04-11 15:39:33 +0800

    add background transparent

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -424,6 +424,41 @@ return {
 		"potamides/pantran.nvim",
 		cmd = "Pantran",
 	},
+	-- Background transparent
+	{
+		"xiyaowong/transparent.nvim",
+		config = function()
+			require("transparent").setup({
+				groups = { -- table: default groups
+					"Normal",
+					"NormalNC",
+					"Comment",
+					"Constant",
+					"Special",
+					"Identifier",
+					"Statement",
+					"PreProc",
+					"Type",
+					"Underlined",
+					"Todo",
+					"String",
+					"Function",
+					"Conditional",
+					"Repeat",
+					"Operator",
+					"Structure",
+					"LineNr",
+					"NonText",
+					"SignColumn",
+					"CursorLineNr",
+					"EndOfBuffer",
+				},
+				extra_groups = {}, -- table: additional groups that should be cleared
+				exclude_groups = {}, -- table: groups you don't want to clear
+				-- enable = true,
+			})
+		end,
+	},
 	-- references
 	{
 		"RRethy/vim-illuminate",
