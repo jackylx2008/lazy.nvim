4c1405078eba8c8367c8b7787ef9a6198039651c
jackylx2008 <jackylx2008@hotmail.com>
2023-06-05 17:45:22 +0800

    null-ls to lualine

--- lua/plugins/configs/lsp/null-ls/init.lua
+++ lua/plugins/configs/lsp/null-ls/init.lua
@@ -1,61 +1,75 @@
--- import null-ls plugin safely
-local setup, null_ls = pcall(require, "null-ls")
-if not setup then
-	return
+local M = {}
+
+local nls = require "null-ls"
+local nls_utils = require "null-ls.utils"
+local b = nls.builtins
+local with_diagnostics_code = function(builtin)
+  return builtin.with {
+    diagnostics_format = "#{m} [#{c}]",
+  }
+end
+-- local refurb = require "config.lsp.null-ls.diagnostics.refurb"
+
+-- local with_root_file = function(builtin, file)
+--   return builtin.with {
+--     condition = function(utils)
+--       return utils.root_has_file(file)
+--     end,
+--   }
+-- end
+
+local sources = {
+  -- formatting
+  b.formatting.prettierd,
+  b.formatting.shfmt,
+  b.formatting.shellharden,
+  b.formatting.fixjson,
+  b.formatting.black.with { extra_args = { "--fast" } },
+  b.formatting.isort,
+  b.formatting.stylua,
+  -- b.formatting.google_java_format,
+  -- with_root_file(b.formatting.stylua, "stylua.toml"),
+
+  -- diagnostics
+  b.diagnostics.write_good,
+  -- b.diagnostics.markdownlint,
+  b.diagnostics.eslint_d,
+  b.diagnostics.flake8.with { extra_args = { "--max-line-length=180" } },
+  -- b.diagnostics.tsc,
+  -- b.diagnostics.selene,
+  -- b.diagnostics.codespell,
+  -- with_root_file(b.diagnostics.selene, "selene.toml"),
+  with_diagnostics_code(b.diagnostics.shellcheck),
+  b.diagnostics.zsh,
+  -- refurb,
+  -- b.diagnostics.cspell.with {
+  --   filetypes = { "python", "rust", "typescript" },
+  -- },
+  -- b.diagnostics.stylelint,
+
+  -- code actions
+  b.code_actions.gitsigns.with {
+    disabled_filetypes = { "NeogitCommitMessage" },
+  },
+  -- b.code_actions.eslint_d,
+  b.code_actions.gitrebase,
+  -- b.code_actions.refactoring,
+  -- b.code_actions.proselint,
+  b.code_actions.shellcheck,
+
+  -- hover
+  b.hover.dictionary,
+}
+
+function M.setup(opts)
+  nls.setup {
+    debug = false,
+    debounce = 150,
+    save_after_format = false,
+    sources = sources,
+    on_attach = opts.on_attach,
+    root_dir = nls_utils.root_pattern ".git",
+  }
 end
 
--- for conciseness
-local formatting = null_ls.builtins.formatting -- to setup formatters
-local diagnostics = null_ls.builtins.diagnostics -- to setup linters
--- to setup format on savrstere
-local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
-
--- configure null_ls
-null_ls.setup({
-	-- setup formatters & linters
-	sources = {
-		--  to disable file types use
-		--  "formatting.prettier.with({disabled_filetypes: {}})" (see null-ls docs)
-		formatting.prettier, -- js/ts formatter
-		formatting.stylua, -- lua formatter
-		formatting.black.with({ extra_args = { "--fast" } }),
-		formatting.clang_format,
-		formatting.isort,
-		diagnostics.luacheck,
-		diagnostics.cpplint.with({
-			-- Get rid of warnings
-			args = {
-				"--filter=-legal/copyright,-readability/todo,-whitespace/comments,-whitespace/indent,-build/c++11,-build/namespaces",
-				"$FILENAME",
-			},
-			-- WARN: "an else should appear on the same line as the preceding"
-		}),
-		diagnostics.flake8,
-		diagnostics.markdownlint,
-		diagnostics.eslint_d.with({ -- js/ts linter
-			-- only enable eslint if root has .eslintrc.js (not in youtube nvim video)
-			condition = function(utils)
-				return utils.root_has_file(".eslintrc.js") -- change file extension if you use something else
-			end,
-		}),
-	},
-	-- configure format on save
-	on_attach = function(current_client, bufnr)
-		if current_client.supports_method("textDocument/formatting") then
-			vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
-			vim.api.nvim_create_autocmd("BufWritePre", {
-				group = augroup,
-				buffer = bufnr,
-				callback = function()
-					vim.lsp.buf.format({
-						filter = function(client)
-							--  only use null-ls for formatting instead of lsp server
-							return client.name == "null-ls"
-						end,
-						bufnr = bufnr,
-					})
-				end,
-			})
-		end
-	end,
-})
+return M
