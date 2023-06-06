b2481754ac91542f0759c029b2ba837217a34f97
Jackylx2008 <jackylx2008@hotmail.com>
2023-06-06 15:27:12 +0800

    add cpp snippets

---
+++ lua/snip/snippets/cpp.lua
@@ -0,0 +1,18 @@
+local ls = require("luasnip")
+local s = ls.snippet
+local t = ls.text_node
+local i = ls.insert_node
+local d = ls.dynamic_node
+local c = ls.choice_node
+local sn = ls.snippet_node
+local isn = ls.indent_snippet_node
+local fmt = require("luasnip.extras.fmt").fmt
+local types = require("luasnip.util.types")
+local events = require("luasnip.util.events")
+local r = ls.restore_node
+
+local snippets = {
+	s("todo", t("// TODO: ")),
+	s("str", t("std::string ")),
+}
+return snippets
