5ad2a632a32ec7723fcabd6f617354d0ffec6959
Jackylx2008 <jackylx2008@hotmail.com>
2023-11-16 14:41:41 +0800

    all ignore msg in flake8

--- lua/plugins/configs/lsp/null-ls/init.lua
+++ lua/plugins/configs/lsp/null-ls/init.lua
@@ -33,7 +33,7 @@ null_ls.setup({
 		diagnostics.flake8.with({
 			-- Get rid of Errors:whitespace after ":"
 			args = {
-				"--ignore=E203,E501,W291,F821,E225,E741",
+				"--ignore=E203,E501,W291,F821,E225,E741,E266",
 			},
 		}),
 		diagnostics.markdownlint,
