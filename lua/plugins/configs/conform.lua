30ef1d16156f6dc5fa3e695d01b83834debd9ddd
Jackylx2008 <jackylx2008@hotmail.com>
2024-05-18 14:10:47 +0800

    Add Trouble.nvim;Before change formatter

---
+++ lua/plugins/configs/conform.lua
@@ -0,0 +1,34 @@
+local status_ok, conform = pcall(require, "conform")
+-- if not status_ok then
+--   return
+-- end
+
+conform.setup({
+	-- Everything in opts will be passed to setup()
+	opts = {
+		-- Define your formatters
+		formatters_by_ft = {
+			lua = { "stylua" },
+			python = { "isort", "black" },
+			javascript = { { "prettierd", "prettier" } },
+			cpp = { { "clang_format" } },
+			markdown = { "prettier" },
+		},
+		-- Set up format-on-save
+		format_on_save = {
+			lsp_fallback = true,
+			async = false,
+			timeout_ms = 1000,
+		},
+		-- Customize formatters
+		formatters = {
+			shfmt = {
+				prepend_args = { "-i", "2" },
+			},
+		},
+	},
+	init = function()
+		-- If you want the formatexpr, here is the place to set it
+		vim.o.formatexpr = "v:lua.require'conform'.formatexpr()"
+	end,
+})
