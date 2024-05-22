1c49c18a4abfa070fb0a90612ecc11086df9ebab
jackylx2008 <jackylx2008@hotmail.com>
2024-05-22 17:12:10 +0800

    lua_ls other setup

--- lua/plugins/configs/lsp/lspconfig.lua
+++ lua/plugins/configs/lsp/lspconfig.lua
@@ -1,16 +1,16 @@
 local lspconfig_status_ok, lspconfig = pcall(require, "lspconfig")
 if not lspconfig_status_ok then
-	return
+  return
 end
 
 local cmp_nvim_lsp_status_ok, cmp_nvim_lsp = pcall(require, "cmp_nvim_lsp")
 if not cmp_nvim_lsp_status_ok then
-	return
+  return
 end
 
 local navic_lsp_status_ok, navic = pcall(require, "nvim-navic")
 if not navic_lsp_status_ok then
-	return
+  return
 end
 
 -- local navbuddy = require("nvim-navbuddy")
@@ -28,33 +28,33 @@ end
 -- end
 
 local on_attach = function(client, bufnr)
-	-- Navic
-	if client.server_capabilities.documentSymbolProvider then
-		navic.attach(client, bufnr)
-	end
-	-- Enable completion triggered by <c-x><c-o>
-	local nmap = function(keys, func, desc)
-		if desc then
-			desc = "LSP: " .. desc
-		end
+  -- Navic
+  if client.server_capabilities.documentSymbolProvider then
+    navic.attach(client, bufnr)
+  end
+  -- Enable completion triggered by <c-x><c-o>
+  local nmap = function(keys, func, desc)
+    if desc then
+      desc = "LSP: " .. desc
+    end
 
-		vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
-	end
+    vim.keymap.set("n", keys, func, { buffer = bufnr, desc = desc })
+  end
 
-	nmap("gd", "<cmd>Lspsaga peek_definition<CR>", "Goto Definition")
-	nmap("gD", "<cmd>Lspsaga goto_definition<CR>", "Goto Declaration")
-	nmap("K", "<cmd>Lspsaga hover_doc<CR>", "Hover Documentation")
-	nmap("gi", "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation")
-	nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
-	-- nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
-	-- nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
-	-- nmap("<leader>wl", function()
-	-- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
-	-- end, "[W]orkspace [L]ist Folders")
-	nmap("gr", "<cmd>Telescope lsp_references<CR>", "[G]oto [R]eferences")
-	nmap("gt", "<cmd>Telescope lsp_type_definitions<CR>", "[G]oto [R]eferences")
-	nmap("[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostic jump pprev")
-	nmap("]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", "Diagnostic jump pnext")
+  nmap("gd", "<cmd>Lspsaga peek_definition<CR>", "Goto Definition")
+  nmap("gD", "<cmd>Lspsaga goto_definition<CR>", "Goto Declaration")
+  nmap("K", "<cmd>Lspsaga hover_doc<CR>", "Hover Documentation")
+  nmap("gi", "<cmd>Telescope lsp_implementations<CR>", "Goto Implementation")
+  nmap("<C-k>", vim.lsp.buf.signature_help, "Signature Documentation")
+  -- nmap("<leader>wa", vim.lsp.buf.add_workspace_folder, "[W]orkspace [A]dd Folder")
+  -- nmap("<leader>wr", vim.lsp.buf.remove_workspace_folder, "[W]orkspace [R]emove Folder")
+  -- nmap("<leader>wl", function()
+  -- 	print(vim.inspect(vim.lsp.buf.list_workspace_folders()))
+  -- end, "[W]orkspace [L]ist Folders")
+  nmap("gr", "<cmd>Telescope lsp_references<CR>", "[G]oto [R]eferences")
+  nmap("gt", "<cmd>Telescope lsp_type_definitions<CR>", "[G]oto [R]eferences")
+  nmap("[d", "<cmd>Lspsaga diagnostic_jump_prev<CR>", "Diagnostic jump pprev")
+  nmap("]d", "<cmd>Lspsaga diagnostic_jump_next<CR>", "Diagnostic jump pnext")
 end
 
 -- used to enable autocompletion (assign to every lsp server config)
@@ -64,92 +64,70 @@ local capabilities = cmp_nvim_lsp.default_capabilities()
 -- (not in youtube nvim video)
 local signs = { Error = " ", Warn = " ", Hint = "ﴞ ", Info = " " }
 for type, icon in pairs(signs) do
-	local hl = "DiagnosticSign" .. type
-	vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
+  local hl = "DiagnosticSign" .. type
+  vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
 end
 
 -- configure clangd server
 -- touch compile_flags.txt and add std=c++17 to clarify c++ std
 capabilities.offsetEncoding = { "utf-16" } -- to avoid multi server warning in c/cpp file
 lspconfig["clangd"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
 -- configure pyright server
 local python_env = {}
 if vim.loop.os_uname().sysname == "Darwin" then
-	-- custom settings for mac
-	python_env = {
-		pythonPath = "/opt/anaconda3/bin/python",
-	}
+  -- custom settings for mac
+  python_env = {
+    pythonPath = "/opt/anaconda3/bin/python",
+  }
 end
 lspconfig["pyright"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
-	filetype = { "python" },
-	settings = {
-		python = python_env,
-	},
+  capabilities = capabilities,
+  on_attach = on_attach,
+  filetype = { "python" },
+  settings = {
+    python = python_env,
+  },
 })
 
 -- configure markdown server
 lspconfig["marksman"].setup({
-	capabilities = capabilities,
-	on_attach = on_attach,
+  capabilities = capabilities,
+  on_attach = on_attach,
 })
 
 -- configure lua server (with special settings)
 lspconfig["lua_ls"].setup({
-	-- capabilities = capabilities,
-	-- on_attach = on_attach,
-	-- settings = { -- custom settings for lua
-	-- 	Lua = {
-	-- 		-- make the language server recognize "vim" global
-	-- 		diagnostics = {
-	-- 			globals = { "vim" },
-	-- 		},
-	-- 		workspace = {
-	-- 			-- make language server aware of runtime files
-	-- 			library = {
-	-- 				[vim.fn.expand("$VIMRUNTIME/lua")] = true,
-	-- 				[vim.fn.stdpath("config") .. "/lua"] = true,
-	-- 			},
-	-- 		},
-	-- 	},
-	-- },
-	capabilities = capabilities,
-	on_init = function(client)
-		local path = client.workspace_folders[1].name
-		if not vim.loop.fs_stat(path .. "/.luarc.json") and not vim.loop.fs_stat(path .. "/.luarc.jsonc") then
-			client.config.settings = vim.tbl_deep_extend("force", client.config.settings, {
-				Lua = {
-					runtime = {
-						-- Tell the language server which version of Lua you're using
-						-- (most likely LuaJIT in the case of Neovim)
-						version = "LuaJIT",
-					},
-					diagnostics = {
-						globals = { "vim" },
-					},
-					-- Make the server aware of Neovim runtime files
-					workspace = {
-						checkThirdParty = false,
-						library = {
-							vim.env.VIMRUNTIME .. "/lua",
-							-- "${3rd}/luv/library"
-							-- "${3rd}/busted/library",
-						},
-						-- or pull in all of 'runtimepath'. NOTE: this is a lot slower
-						-- library = vim.api.nvim_get_runtime_file("", true)
-					},
-				},
-			})
-
-			client.notify("workspace/didChangeConfiguration", { settings = client.config.settings })
-		end
-		return true
-	end,
+  -- from https://github.com/Shaobin-Jiang/IceNvim/blob/master/lua/plugins/lsp.lua
+  -- WARN: don't work
+  on_attach = function()
+    local runtime_path = vim.split(package.path, ";")
+    table.insert(runtime_path, "lua/?.lua")
+    table.insert(runtime_path, "lua/?/init.lua")
+    return vim.tbl_extend("force", config.default(), {
+      settings = {
+        Lua = {
+          runtime = {
+            version = "LuaJIT",
+            path = runtime_path,
+          },
+          diagnostics = {
+            globals = { "vim" },
+          },
+          workspace = {
+            library = vim.api.nvim_get_runtime_file("", true),
+            checkThirdParty = false,
+          },
+          telemetry = {
+            enable = false,
+          },
+        },
+      },
+    })
+  end,
 })
 
 -- configure rust_analyzer server
